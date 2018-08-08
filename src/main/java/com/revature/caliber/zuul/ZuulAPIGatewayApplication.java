98c50679a8df35fa78dc780a3bb2ccfc7cd0cccb
Ezzdean Bietar <EzzdeanB@gmail.com>
2018-08-08 09:04:09 -0400

    Current status: not working

--- src/main/java/com/revature/caliber/zuul/ZuulAPIGatewayApplication.java
+++ src/main/java/com/revature/caliber/zuul/ZuulAPIGatewayApplication.java
@@ -3,10 +3,13 @@ package com.revature.caliber.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableHystrix
 @EnableDiscoveryClient
 public class ZuulAPIGatewayApplication {
 
