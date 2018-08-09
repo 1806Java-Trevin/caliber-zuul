2e61d4d61cd56c36ce64afee9582387e1af00534
Kea <KeaUehara@gmail.com>
2018-08-09 14:32:16 -0400

    Changed format of test

--- src/test/java/com/revature/caliber/zuul/MainTest.java
+++ src/test/java/com/revature/caliber/zuul/MainTest.java
@@ -17,7 +17,7 @@ public class MainTest {
 	@Test
 	public void testMain() {
 		try {
-			ZuulAPIGatewayApplication.main(new String[] { "Test" });
+			app.main(new String[] { "Test" });
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
